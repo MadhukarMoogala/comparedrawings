3a67f3cf3c00b86473cf2a02cc717d3bf57d3ca5
Madhukar Moogala <madhukar.moogala@autodesk.com>
2022-02-03 16:00:07 +0530

    Updated Activitysign

--- src/utils.js
+++ src/utils.js
@@ -8,5 +8,5 @@ var FORGE_USER_URL =
   "https://developer.api.autodesk.com/userprofile/v1/users/@me";
 var FORGE_WEBSOCKET_URL = "wss://websockets.forgedesignautomation.io";
 var FORGE_ACTIVITY_ID = "fpdmad.DwgCompareInplace+prod";
-var FORGE_ACTIVITY_SIGN =
-  "jKhaeBuKBq6VQx74ovlU7Gmew5MFRBrGjDQibz7lkgQNVb5yr7iMPuTZOdKcExHyPEWu0Zq+UaqUuSWnlhwa3DdxQecq4tLHJOgcqxog3W3HQQSDxH+YAdjK31IELRKCED7+qyOksUps0qt3SrhWDSKhjk2qvLIeM6kRJU/jdjvrFK/eDFQxOIpPjL8vBbeLc8p06zIbVNdE315iRoTkP2Q7EWu0soqI4dKcb3iPcClIOfExydTE8a7S3ttGDTOrAjdGVcfGzbz487EcrGPcNYVnkRIIvNGvC7HNNwdi4g1jUjTkm6vcfiIRX6fAepdrfujnepDsjeVEpgJa0Y7RRQ==";
+var FORGE_ACTIVITY_SIGN = "xU/mJQQRsZ2RJYwieUt80uYX3ihxBSTPsdoEi6pc61IosiTvBEDQ97tmZTUyXXcgF5lG+8BSQ6SwxK10572+1+9r2lkcySEDMAzAkhZfbx8Gno/yAfBPcfqZuLyOd1DjsLgZXN42xUznv2FTZGZ+uFeQLgpouVdPJ32N1zAsgGOZ3VpQcqQKF1hGXDFcQo+/MXLhu4aZ26H8qNwnKSacfMVWMZPVfoxAaaJRBmScHSGF0lD30oCv05LtoOS4MMUloGgrrV5v9k7Ftpkcg9D6I1HzrR+SjHbrgkB0CtMg/GeoBXnaOXYGbdU9wqjIIDSja7c79JWBga++HTUYi5bcQw=="
+  
